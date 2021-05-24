2fef245075bd0191ebb9f5c3b4c3f8404b41e4ea
jaekwon.bang <jaekwon.bang@lge.com>
2021-05-24 17:53:12 +0900

    Changme help message order

--- src/fosslight_dependency/_help.py
+++ src/fosslight_dependency/_help.py
@@ -19,19 +19,20 @@ _HELP_MESSAGE_DEPENDENCY = """
         Pub : Flutter_oss_licenses
 
     Options:
+        Optional
+            -h\t\t\t\t    Print help message.
+            -v\t\t\t\t    Print the version of the script.
+            -m <package_manager>\t    Enther the package manager(npm, maven, gradle, pip, pub, cocoapods).
+            -p <input_path>\t\t    Enter the path where the script will be run.
+            -o <output_path>\t\t    Enter the path where the result file will be generated.
+        
         Required only for pypi
             -a <activate_cmd>\t\t    Virtual environment activate command(ex, 'conda activate (venv name)')
             -d <deactivate_cmd>\t\t    Virtual environment deactivate command(ex, 'conda deactivate')
             
         Optional only for gradle, maven
             -c <dir_name>\t\t    Enter the customized build output directory name(default: target)
-
-        Optional
-            -h\t\t\t\t    Print help message.
-            -v\t\t\t\t    Print the version of the script.
-            -m <package_manager>\t    Enther the package manager(npm, maven, gradle, pip...).
-            -p <input_path>\t\t    Enter the path where the script will be run.
-            -o <output_path>\t\t    Enter the path where the result file will be generated."""
+        """
 
 
 def print_help_msg():
