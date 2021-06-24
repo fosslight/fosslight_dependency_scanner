a350470b2c0dfd1ac6a944fd268b129950e64fa1
Jaekwon Bang <jaekwon.bang@lge.com>
2021-06-24 18:23:30 +0900

    Add Copyright text to hook-fosslight_dependency.py file
    Signed-off-by: Jaekwon Bang <jaekwon.bang@lge.com>

--- hooks/hook-fosslight_dependency.py
+++ hooks/hook-fosslight_dependency.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (c) 2021 LG Electronics Inc.
+# SPDX-License-Identifier: Apache-2.0
 from PyInstaller.utils.hooks import collect_all
 
 datas, binaries, hiddenimports = collect_all('fosslight_dependency')
