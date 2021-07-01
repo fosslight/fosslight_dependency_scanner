316d652da41938da68eb50a6f2ad7a1e2e1f43da
jiyeong.seok <jiyeong.seok@lge.com>
2021-07-01 18:03:12 +0900

    Change the mvn wrapper command and fix the pyinstaller error
    Signed-off-by: Jiyeong Seok <jiyeong.seok@lge.com>

--- hooks/hook-fosslight_dependency.py
+++ hooks/hook-fosslight_dependency.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 # Copyright (c) 2021 LG Electronics Inc.
 # SPDX-License-Identifier: Apache-2.0
-from PyInstaller.utils.hooks import collect_all
+from PyInstaller.utils.hooks import copy_metadata, collect_all
 
 datas, binaries, hiddenimports = collect_all('fosslight_dependency')
+datas += copy_metadata('fosslight_dependency')
