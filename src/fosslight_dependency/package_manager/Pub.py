99ad2859b0e7f87a360fa1b12ed14d7a28f7a70b
Jiyeong Seok <jiyeong.seok@lge.com>
2022-07-01 12:21:17 +0900

    Modify to analyze pub dependency
    Signed-off-by: Jiyeong Seok <jiyeong.seok@lge.com>

--- src/fosslight_dependency/package_manager/Pub.py
+++ src/fosslight_dependency/package_manager/Pub.py
@@ -7,6 +7,9 @@ import os
 import logging
 import json
 import re
+import shutil
+import yaml
+import subprocess
 import fosslight_util.constant as constant
 import fosslight_dependency.constant as const
 from fosslight_dependency._package_manager import PackageManager
@@ -19,11 +22,64 @@ class Pub(PackageManager):
     package_manager_name = const.PUB
 
     dn_url = 'https://pub.dev/packages/'
-    input_file_name = os.path.join('lib', 'oss_licenses.dart')
+    input_file_name = 'tmp_flutter_oss_licenses.json'
+    tmp_dir = "fl_dependency_tmp_dir"
+    cur_path = ''
 
     def __init__(self, input_dir, output_dir):
         super().__init__(self.package_manager_name, self.dn_url, input_dir, output_dir)
         self.append_input_package_list_file(self.input_file_name)
+
+    def __del__(self):
+        if self.cur_path != '':
+            os.chdir(self.cur_path)
+        if os.path.exists(self.tmp_dir):
+            shutil.rmtree(self.tmp_dir)
+
+    def run_plugin(self):
+        if not os.path.exists(const.SUPPORT_PACKAE.get(self.package_manager_name)):
+            logger.error(f"Cannot find the file({const.SUPPORT_PACKAE.get(self.package_manager_name)})")
+            return False
+
+        if os.path.exists(self.tmp_dir):
+            shutil.rmtree(self.tmp_dir)
+        os.mkdir(self.tmp_dir)
+        shutil.copy(const.SUPPORT_PACKAE.get(self.package_manager_name),
+                    os.path.join(self.tmp_dir, const.SUPPORT_PACKAE.get(self.package_manager_name)))
+
+        self.cur_path = os.getcwd()
+        os.chdir(self.tmp_dir)
+
+        with open(const.SUPPORT_PACKAE.get(self.package_manager_name), 'r', encoding='utf8') as f:
+            tmp_yml = yaml.safe_load(f)
+            tmp_yml['dev_dependencies'] = {'flutter_oss_licenses': '^2.0.1'}
+        with open(const.SUPPORT_PACKAE.get(self.package_manager_name), 'w', encoding='utf8') as f:
+            f.write(yaml.dump(tmp_yml))
+
+        cmd = "flutter pub get"
+        ret = subprocess.call(cmd, shell=True)
+        if ret != 0:
+            logger.error(f"Failed to run: {cmd}")
+            os.chdir(self.cur_path)
+            return False
+        cmd = "flutter pub deps --no-dev"
+        ret = subprocess.check_output(cmd, shell=True)
+        if ret != 0:
+            pub_deps = ret.decode('utf8')
+            for line in pub_deps.split('\n'):
+                re_result = re.findall(r'\-\-\s(\S+[^.\s])[.|\s]', line)
+                if re_result:
+                    self.total_dep_list.append(re_result[0])
+            self.total_dep_list = list(set(self.total_dep_list))
+
+        cmd = f"flutter pub run flutter_oss_licenses:generate.dart -o {self.input_file_name} --json"
+        ret = subprocess.call(cmd, shell=True)
+        if ret != 0:
+            logger.error(f"Failed to run: {cmd}")
+            os.chdir(self.cur_path)
+            return False
+
+        return True
 
     def parse_oss_information(self, f_name):
         tmp_license_txt_file_name = 'tmp_license.txt'
@@ -31,43 +87,41 @@ class Pub(PackageManager):
         comment = ''
 
         with open(f_name, 'r', encoding='utf8') as pub_file:
-            json_txt = preprocess_pub_result(pub_file)
-            if json_txt:
-                json_data = json.loads(json_txt)
+            json_f = json.load(pub_file)
 
         try:
             sheet_list = []
 
-            for key in json_data:
-                oss_origin_name = json_data[key]['name']
-                oss_name = f"{self.package_manager_name}:{oss_origin_name}"
-                oss_version = json_data[key]['version']
-                homepage = json_data[key]['homepage']
-                dn_loc = f"{self.dn_url}{oss_origin_name}/versions/{oss_version}"
-                license_txt = json_data[key]['license']
+            for json_data in json_f:
+                oss_origin_name = json_data['name']
+                if oss_origin_name in self.total_dep_list:
+                    oss_name = f"{self.package_manager_name}:{oss_origin_name}"
+                    oss_version = json_data['version']
+                    homepage = json_data['homepage']
+                    dn_loc = f"{self.dn_url}{oss_origin_name}/versions/{oss_version}"
+                    license_txt = json_data['license']
 
-                tmp_license_txt = open(tmp_license_txt_file_name, 'w', encoding='utf-8')
-                tmp_license_txt.write(license_txt)
-                tmp_license_txt.close()
+                    tmp_license_txt = open(tmp_license_txt_file_name, 'w', encoding='utf-8')
+                    tmp_license_txt.write(license_txt)
+                    tmp_license_txt.close()
 
-                license_name_with_license_scanner = check_and_run_license_scanner(self.platform,
-                                                                                  self.license_scanner_bin,
-                                                                                  tmp_license_txt_file_name)
+                    license_name_with_license_scanner = check_and_run_license_scanner(self.platform,
+                                                                                      self.license_scanner_bin,
+                                                                                      tmp_license_txt_file_name)
 
-                if license_name_with_license_scanner != "":
-                    license_name = license_name_with_license_scanner
-                else:
-                    license_name = ''
-
-                if self.direct_dep:
-                    if json_data[key]['isDirectDependency']:
-                        comment = 'direct'
+                    if license_name_with_license_scanner != "":
+                        license_name = license_name_with_license_scanner
                     else:
-                        comment = 'transitive'
+                        license_name = ''
 
-                sheet_list.append([const.SUPPORT_PACKAE.get(self.package_manager_name),
-                                  oss_name, oss_version, license_name, dn_loc, homepage, '', '', comment])
+                    if self.direct_dep:
+                        if json_data['isDirectDependency']:
+                            comment = 'direct'
+                        else:
+                            comment = 'transitive'
 
+                    sheet_list.append([const.SUPPORT_PACKAE.get(self.package_manager_name),
+                                      oss_name, oss_version, license_name, dn_loc, homepage, '', '', comment])
         except Exception as e:
             logger.error(f"Fail to parse pub oss information: {e}")
 
@@ -78,12 +132,3 @@ class Pub(PackageManager):
 
     def parse_direct_dependencies(self):
         self.direct_dep = True
-
-
-def preprocess_pub_result(input_file):
-    matched_json = re.findall(r'final ossLicenses = <String, dynamic>({[\s\S]*});', input_file.read())
-    if len(matched_json) > 0:
-        return matched_json[0]
-    else:
-        logger.error("Fail to parse the result json from pub input file.")
-        return ''
