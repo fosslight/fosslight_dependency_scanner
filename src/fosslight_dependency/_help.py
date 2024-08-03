309caed035bccc2656f1d9b8bb306405255ea371
fhdufhdu <fhdufhdu@gmail.com>
2024-08-03 15:55:48 +0900

    Update Help Message
    Signed-off-by: fhdufhdu <fhdufhdu@gmail.com>

--- src/fosslight_dependency/_help.py
+++ src/fosslight_dependency/_help.py
@@ -37,9 +37,9 @@ _HELP_MESSAGE_DEPENDENCY = """
             \t\t\t\t\t(If you want to generate the specific file name, add the output path with file name.)
             -f <format> [<format> ...]\t    Output formats (excel, csv, opossum, yaml, spdx-tag, spdx-yaml, spdx-json, spdx-xml)
         \t\t\t\t    Multiple formats can be specified separated by space.
-            --graph_path <save_path> \t    Enter the path where the graph image will be saved
+            --graph-path <save_path> \t    Enter the path where the graph image will be saved
             \t\t\t\t\t(ex. /your/directory/path/filename.{pdf, jpg, png}) (recommend pdf extension)
-            --graph_size <width> <height>   Enter the size of the graph image (The size unit is pixels)
+            --graph-size <width> <height>   Enter the size of the graph image (The size unit is pixels)
             \t\t\t\t\t--graph-path option is required
             --direct\t\t\t    Print the direct/transitive dependency type in comment.
                                 \t\tChoice 'True' or 'False'. (default:True)
