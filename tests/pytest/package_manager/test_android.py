f02c3651ced423d2d16fa6126d848033d7150b4f
yongjunhong <kevin0928@naver.com>
2024-10-02 11:47:42 +0900

    Remove shell option
    Signed-off-by: yongjunhong <kevin0928@naver.com>

--- tests/pytest/package_manager/test_android.py
+++ tests/pytest/package_manager/test_android.py
@@ -15,7 +15,7 @@ DIST_PATH = os.path.join(os.environ.get("TOX_PATH"), "dist", "cli.exe")
 @pytest.mark.ubuntu
 def test_ubuntu(input_path, output_path, extra_args):
     command = f"fosslight_dependency -p {input_path} -o {output_path} {extra_args}"
-    result = subprocess.run(command, shell=True, capture_output=True, text=True)
+    result = subprocess.run(command, capture_output=True, text=True)
     assert result.returncode == 0, f"Command failed: {command}\nstdout: {result.stdout}\nstderr: {result.stderr}"
     assert any(os.scandir(output_path)), f"Output file does not exist: {output_path}"
 
@@ -26,6 +26,6 @@ def test_ubuntu(input_path, output_path, extra_args):
 @pytest.mark.windows
 def test_windows(input_path, output_path):
     command = f"{DIST_PATH} -p {input_path} -o {output_path}"
-    result = subprocess.run(command, shell=True, capture_output=True, text=True)
+    result = subprocess.run(command, capture_output=True, text=True)
     assert result.returncode == 0, f"Command failed: {command}\nstdout: {result.stdout}\nstderr: {result.stderr}"
     assert any(os.scandir(output_path)), f"Output file does not exist: {output_path}"
--- tests/pytest/package_manager/test_pub.py
+++ tests/pytest/package_manager/test_pub.py
@@ -16,7 +16,7 @@ DIST_PATH = os.path.join(os.environ.get("TOX_PATH"), "dist", "cli.exe")
 @pytest.mark.ubuntu
 def test_ubuntu(input_path, output_path):
     command = f"fosslight_dependency -p {input_path} -o {output_path}"
-    result = subprocess.run(command, shell=True, capture_output=True, text=True)
+    result = subprocess.run(command, capture_output=True, text=True)
     assert result.returncode == 0, f"Command failed: {command}\nstdout: {result.stdout}\nstderr: {result.stderr}"
     assert any(os.scandir(output_path)), f"Output file does not exist: {output_path}"
 
@@ -29,6 +29,6 @@ def test_ubuntu(input_path, output_path):
 @pytest.mark.windows
 def test_windows(input_path, output_path, extra_args):
     command = f"{DIST_PATH} -p {input_path} -o {output_path} {extra_args}"
-    result = subprocess.run(command, shell=True, capture_output=True, text=True)
+    result = subprocess.run(command, capture_output=True, text=True)
     assert result.returncode == 0, f"Command failed: {command}\nstdout: {result.stdout}\nstderr: {result.stderr}"
     assert any(os.scandir(output_path)), f"Output file does not exist: {output_path}"
