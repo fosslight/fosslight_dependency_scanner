f2d8ffe9c379518f26d4f30169ba3bb5645b26ea
rewrite0w0 <rewrite0w0@gmail.com>
2024-10-24 14:28:16 +0900

    chore: flake8에 맞춰 코드 정리한다.
    Signed-off-by: rewrite0w0 <rewrite0w0@gmail.com>

--- tests/pytest/package_manager/test_mod.py
+++ tests/pytest/package_manager/test_mod.py
@@ -6,7 +6,9 @@ import os
 import pytest
 import subprocess
 
+
 DIST_PATH = os.path.join(os.environ.get("TOX_PATH"), "dist", "cli.exe")
+
 
 @pytest.mark.parametrize("input_path, output_path, extra_args", [
     ("tests/test_mod", "tests/result/mod", "-m mod")
@@ -17,6 +19,7 @@ def test_ubuntu(input_path, output_path, extra_args):
     result = subprocess.run(command, shell=True, capture_output=True, text=True)
     assert result.returncode == 0, f"Command failed: {command}\nstdout: {result.stdout}\nstderr: {result.stderr}"
     assert any(os.scandir(output_path)), f"Output file does not exist: {output_path}"
+
 
 @pytest.mark.parametrize("input_path, output_path", [
     (os.path.join("tests", "test_mod"), os.path.join("tests", "result", "mod")),
