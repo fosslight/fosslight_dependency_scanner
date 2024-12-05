b8bcdb432c22aea89616030e0cbbe0a69cc02faa
Jiyeong Seok <50347670+dd-jy@users.noreply.github.com>
2024-12-05 10:40:15 +0900

    Support cycloneDX format (#235)
    Signed-off-by: jiyeong.seok <jiyeong.seok@lge.com>

--- src/fosslight_dependency/run_dependency_scanner.py
+++ src/fosslight_dependency/run_dependency_scanner.py
@@ -128,12 +128,15 @@ def run_dependency_scanner(package_manager='', input_dir='', output_dir_file='',
             to_remove = []  # elements of spdx format on windows that should be removed
             for i, output_extension in enumerate(output_extensions):
                 if formats:
-                    if formats[i].startswith('spdx'):
-                        if platform.system() != 'Windows':
-                            output_files[i] = f"fosslight_spdx_dep_{_start_time}"
-                        else:
-                            logger.warning('spdx format is not supported on Windows. Please remove spdx from format.')
+                    if formats[i].startswith('spdx') or formats[i].startswith('cyclonedx'):
+                        if platform.system() == 'Windows':
+                            logger.warning(f'{formats[i]} is not supported on Windows.Please remove {formats[i]} from format.')
                             to_remove.append(i)
+                        else:
+                            if formats[i].stasrtswith('spdx'):
+                                output_files[i] = f"fosslight_spdx_dep_{_start_time}"
+                            elif formats[i].startswith('cyclonedx'):
+                                output_files[i] = f'fosslight_cyclonedx_dep_{_start_time}'
                     else:
                         if output_extension == _json_ext:
                             output_files[i] = f"fosslight_opossum_dep_{_start_time}"
